5d80726bf2225b570b29a65148c8fac75ce9bdd4
komplekt17 <komplekt_17@mail.ru>
2020-01-14 11:17:45 +0500

    screenshot added

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const yenv = require('yenv');
 const env = yenv('env.yaml', { env: 'development' });
 
+// libs screenshots by js
+// https://www.google.com/search?newwindow=1&rlz=1C1CHBD_ruRU786RU787&sxsrf=ACYBGNQE8qdNiJCuTgAFumJq8w06ylBlag%3A1578772170682&ei=yiYaXtmnKaSurgTe1JbQCA&q=js+screenshot+by+link&oq=js+screenshot+by+link&gs_l=psy-ab.12...353086.357372..359879...0.2..0.190.1297.0j8......0....1..gws-wiz.......0i71j0i67j0j0i22i30j33i22i29i30j33i160.tMHI-f83yV4&ved=0ahUKEwjZtKjTqPzmAhUkl4sKHV6qBYoQ4dUDCAs
+
 const app = express();
 const port = env.NODE_PORT;
 
